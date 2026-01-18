5d2d045bfb1d82d4ee4ee6fb5569a6c7cd1a3423
CoffeeMach <you@example.com>
2026-01-18 19:39:40 +0900

    funny if else example

--- prog.c
+++ prog.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 int main() {
-    bool iamstupid = true;
+    bool iamstupid;
 
-    char name[15] = "";
+    char name[15];
+
+    printf("Enter your name: ");
+    scanf("%14s", name);
+
+    char answer[4];
+
+    printf("Are you an idiot? Answer 'yes' or 'no': ");
+    scanf("%3s", answer);
+
+    if (strcmp(answer, "yes") == 0) { iamstupid = true; } else { iamstupid = false; }
 
     if (iamstupid) {
-        printf("Enter your name: ");
-        scanf("%14s", name);
         printf("%s is an idiot.", name);
+    } else {
+        printf("%s thinks they are not an idiot.", name);
     }
 
     return 0;
