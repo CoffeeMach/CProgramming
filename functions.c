68b069e0f737fa6dee5fa0b9d3e9ea8d1ccc23bd
CoffeeMach <you@example.com>
2026-01-30 04:55:17 +0900

    Java-like programs using fun prototype

--- functions.c
+++ functions.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 
-void greet(char name[15]) {  //does not return any value
-    printf("Good morning %s", name);
-}
+//function prototypes to define functions after main
+void greet(char name[]);
+int square(int number);
 
 int main() {
     char username[15];
     printf("What is your name?: ");
     scanf("%14s", username);
     greet(username);
+
+    int result = square(4);
+    printf("\nResult = %d", result);
+
     return 0;
+}
+
+void greet(char name[]) {  //does not return any value
+    printf("Good morning %s", name);
+}
+
+int square(int number) {  //stores return value
+    int square = number*number;
+    return square;
 }
