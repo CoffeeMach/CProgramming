e1ab370b0479a132eab1ac43fb45ad33ada1d07c
CoffeeMach <you@example.com>
2026-01-31 04:14:38 +0900

    Using indirect recursion

--- recursion.c
+++ recursion.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 int factorial(int number);
+int doublefact(int number);
+void oddcalc(int oddnum);
+void evencalc(int evennum);
+void WAP(int n);
 
 int main() {
-    int result = factorial(5);
-    printf("%d", result);
+    // int result = factorial(5);
+    // int result2 = doublefact(5);
+    // // printf("%d", result);
+    // printf("%d", result2);
+
+    WAP(10);
     return 0;
 }
 
@@ -13,5 +21,25 @@ int factorial(int number) {
         return 1;
     } else {
         return number*factorial(number-1);
+    }
+}
+
+void oddcalc(int oddnum) {
+    WAP(oddnum-1);
+    printf(" %d", oddnum + 1);
+}
+
+void evencalc(int evennum) {
+    WAP(evennum-1);
+    printf(" %d", evennum - 1);
+}
+
+void WAP(int n) {
+    if (n == 0) {
+        return;
+    } else if (n % 2 == 0) {
+        evencalc(n);
+    } else {
+        oddcalc(n);
     }
 }
